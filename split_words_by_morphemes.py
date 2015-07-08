2867bc2f99eb6b1e16ca56eebb687cd5a4659c75
tuur <t.leeuwenberg@gmail.com>
2015-07-08 23:49:54 +0200

    Create split_words_by_morphemes.py
    Simple python2.7 wrapper to build (or read) a morfessor baseline model, and use it to segment all words in a corpus.
    Requirements: morfessor installed for python (https://pypi.python.org/pypi/Morfessor)
    
    README:
    
    usage: split_morphemes.py [-h] [-model model] [-output_corpus output_corpus]
                              [-save_model save_model] [-maxepochs maxepochs]
                              corpus
    
    Trains a morfessor model given a corpus, and splits all words in the corpus
    according to the morfessor model. You can also provide a Morfessor binary
    model file.
    
    positional arguments:
      corpus                The text corpus, one sentence per line
    
    optional arguments:
      -h, --help            show this help message and exit
      -model model          Morfessor model file path (binary file)
      -output_corpus output_corpus
                            The output corpus file path
      -save_model save_model
                            Saves the model as a binary model file (provide file
                            path)
      -maxepochs maxepochs  Maximum iterations for training the model (default= no
                            maximum, stop untill convergence)

---
+++ split_words_by_morphemes.py
@@ -0,0 +1,84 @@
+import morfessor as m
+import argparse
+import sys
+import io
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Trains a morfessor model given a corpus, and splits all words in the corpus according to the morfessor model. You can also provide a Morfessor binary model file.')
+    parser.add_argument('corpus', metavar='corpus', type=str,
+                   help='The text corpus, one sentence per line')
+    parser.add_argument('-model', metavar='model', type=str,default = None,
+                   help='Morfessor model file path (binary file)')
+    parser.add_argument('-output_corpus', metavar='output_corpus', type=str, default=None,
+                   help='The output corpus file path')
+    parser.add_argument('-save_model', metavar='save_model', type=str, default=None,
+                   help='Saves the model as a binary model file (provide file path)')
+    parser.add_argument('-maxepochs', metavar='maxepochs', type=int, default=None,
+                   help='Maximum iterations for training the model (default= no maximum, stop untill convergence)')
+    args = parser.parse_args()
+    mio = m.io.MorfessorIO()
+    
+    corpus = mio.read_corpus_file(args.corpus)
+    
+    # If a model is provided, load the model
+    if args.model: 
+        sys.stderr.write("reading Morfessor model...\n")
+        model = mio.read_any_model(args.model)
+    # If not, train, and possibly save the model
+    else:
+        sys.stderr.write("-- training Morfessor model --\n")
+        sys.stderr.write("reading corpus\n")
+        model = m.baseline.BaselineModel()
+        model.load_data(corpus)
+        sys.stderr.write("training model\n")
+        if args.maxepochs:
+            sys.stderr.write('max epochs:' + str(args.maxepochs)+"\n")
+            model.train_batch(max_epochs=args.maxepochs)
+        else:
+            model.train_batch()
+        if args.save_model:
+            sys.stderr.write("writing model to file:"+(args.save_model)+"\n")
+            mio.write_binary_model_file(args.save_model, model)
+
+    corpus = [s.split() for s in io.open(args.corpus, 'r').read().split('\n') if s!=""]        
+    
+    output = ""
+    
+    for sentence in corpus:
+        out = []
+        for word in sentence:
+            segmentation = model.segment(word)
+            for segment in segmentation:
+                out += segmentation
+        if args.output_corpus:
+            output += ' '.join(out) + '\n' 
+        else:
+            print ' '.join(out).encode('utf-8')
+            
+    if args.output_corpus:
+        sys.stderr.write('writing segmented corpus to'+args.output_corpus+'\n')
+        fout = open(args.output_corpus, 'w')
+        fout.write(output.encode('utf-8'))
+        fout.close()
+        
+usage: split_morphemes.py [-h] [-model model] [-output_corpus output_corpus]
+                          [-save_model save_model] [-maxepochs maxepochs]
+                          corpus
+
+Trains a morfessor model given a corpus, and splits all words in the corpus
+according to the morfessor model. You can also provide a Morfessor binary
+model file.
+
+positional arguments:
+  corpus                The text corpus, one sentence per line
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -model model          Morfessor model file path (binary file)
+  -output_corpus output_corpus
+                        The output corpus file path
+  -save_model save_model
+                        Saves the model as a binary model file (provide file
+                        path)
+  -maxepochs maxepochs  Maximum iterations for training the model (default= no
+                        maximum, stop untill convergence)
